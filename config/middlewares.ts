ee65a3dab7bc7a75a82cfc01c3658e4f6563d188
gosangam <sangam4742@gmail.com>
2025-12-26 15:32:31 +0530

    feat: add cloudflare r2

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,36 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "script-src": [
+            "'self'",
+            "unsafe-inline",
+            "https://maps.googleapis.com",
+          ],
+          "media-src": [
+            "'self'",
+            "blob:",
+            "data:",
+            "s3.prabhupadasays.com",
+            "a7d04b7578371b28c4c80447ec0854b9.r2.cloudflarestorage.com"
+          ],
+          "img-src": [
+            "'self'",
+            "blob:",
+            "data:",
+            "s3.prabhupadasays.com",
+            "a7d04b7578371b28c4c80447ec0854b9.r2.cloudflarestorage.com"
+          ],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
